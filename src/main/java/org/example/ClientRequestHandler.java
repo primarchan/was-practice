8a1356a3c2c3a31dd2556743bc4221e3d9088bd6
An T.G <boltcarrier@naver.com>
2022-12-24 01:25:02 +0900

    웹 애플리케이션 서버 실습 - Step2 사용자 요청이 들어올 때마다 Thread 를 새로 생성해서 사용자 요청을 처리

---
+++ src/main/java/org/example/ClientRequestHandler.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.example.calculator.domain.Calculator;
+import org.example.calculator.domain.PositiveNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ClientRequestHandler implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientRequestHandler.class);
+    private final Socket clientSocket;
+
+    public ClientRequestHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+
+        logger.info("[ClientRequestHandler] new client {} started.", Thread.currentThread().getName());
+
+        try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8)); // line by line 으로 읽어들이기
+            DataOutputStream dos = new DataOutputStream(out);
+
+            HttpRequest httpRequest = new HttpRequest(br);
+
+            if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
+                QueryStrings queryStrings = httpRequest.getQueryString();
+
+                int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
+                String operator = queryStrings.getValue("operator");
+                int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
+
+                int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+                byte[] body = String.valueOf(result).getBytes();
+
+                HttpResponse httpResponse = new HttpResponse(dos);
+                httpResponse.response200Header("application/json", body.length);
+                httpResponse.responseBody(body);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+}
